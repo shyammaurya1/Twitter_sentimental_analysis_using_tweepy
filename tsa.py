f3eb0066a83d764bfef049c6b45c6d10e9be84f8
shyam maurya <shyammaurya395@gmail.com>
2020-02-09 23:55:08 +0530

    Create tsa.py
    remaining part :
    enter your consumer and access keys !

---
+++ tsa.py
@@ -0,0 +1,20 @@
+#Twitter sentimental analysis
+
+import tweepy
+from textblob import TextBlob
+
+consumer_key=''
+consumer_secret=''
+
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+public_tweets = api.search('Trump')
+
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = Text
